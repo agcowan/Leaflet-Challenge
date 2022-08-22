8fe000cf83a139490204b0cbbdac82b6640258bc
Andrew Cowan <100159853+agcowan@users.noreply.github.com>
2022-08-22 12:42:45 -0400

    pushed finished part 1

--- Leaflet-Part-1/logic2.js
+++ Leaflet-Part-1/logic2.js
@@ -1,85 +1,130 @@
-// // Query url
-// var queryUrl = "https://earthquake.usgs.gov/fdsnws/event/1/query?format=geojson&starttime=2022-07-04&endtime=2022-07-05";
+
+// Store our API endpoint as queryUrl.
+var queryUrl = "https://earthquake.usgs.gov/fdsnws/event/1/query?format=geojson&starttime=2022-07-06&endtime=2022-07-07";
 
 
-// let streets = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-//   attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'})
+d3.json(queryUrl).then(function(data){
 
-// let topo = L.tileLayer('https://{s}.tile.opentopomap.org/{z}/{x}/{y}.png', {
-//   attribution: 'Map data: &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, <a href="http://viewfinderpanoramas.org">SRTM</a> | Map style: &copy; <a href="https://opentopomap.org">OpenTopoMap</a> (<a href="https://creativecommons.org/licenses/by-sa/3.0/">CC-BY-SA</a>)'
-// })
+  function createFeatures(earthquakeData) {
 
-// let baseMaps = {
-//   "Streets": streets,
-//   "Topography": topo
-// }
+    // Define a function that we want to run once for each feature in the features array.
+    // Give each feature a popup that describes the place and time of the earthquake.
+    // function onEachFeature(feature, layer) {
+    //   layer.bindPopup(`<h3>${feature.properties.place}</h3><hr><p>${new Date(feature.properties.time)}</p><hr><p>${feature.properties.mag}</p>`);
+    // }
 
-// let map = L.map(map,{
-//   center: [39.5, -98.5],
-//   zoom: 3,
-//   layers: [streets]
-// })
+    function getColor(magnitude){
+      var color = "";
 
-// L.control.layers(baseMaps).addTo(map)
+      //long if-else statement based on magnitude
+      if(magnitude > 5) {
+        color = "#ED1717"
+      } else if(magnitude > 4) {
+        color = "#F24D11"
+      } else if(magnitude > 3) {
+        color = "#F6830C"
+      } else if(magnitude > 2) {
+        color = "#FBB806"
+      } else if(magnitude > 1){
+        color = "#FFEE00"
+      } else {
+        color = "#00FF00"
+      }
+      return color;
+    }
 
-// d3.json(queryUrl).then(function (data) {
-//   L.geoJSON(data).addTo(map)
-// });
+
+    // Create the style data for the earthquakes represented by circles. Passing the magnitude of each to calculate the radius
+    function circleStyle(color,radius) {
+      return {
+        color:color,
+        opacity: 1,
+        fillOpacity: .8,
+        fillColor: color,
+        radius: radius,
+        stroke: true,
+        weight: 1
+      };
+    }
+
+    // Create the circles of each earthquake
+    
 
 
-let queryUrl = "https://earthquake.usgs.gov/fdsnws/event/1/query?format=geojson&starttime=2022-07-04&endtime=2022-07-05";
+    // draw the circle for the earthquake based on the magnitude
+    function getRadius(magnitude) {
+      if (magnitude === 0) {
+        return 1;
+      }
+      return magnitude * 5
+    }
 
-let streets = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-  attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'})
+    
 
-let topo = L.tileLayer('https://{s}.tile.opentopomap.org/{z}/{x}/{y}.png', {
+    L.geoJSON(earthquakeData, { //
+      // onEachFeature: onEachFeature,
+      pointToLayer: function createCircles(feature){
+        console.log(feature)
+        magnitude = feature.properties.mag; //*********************
+        var radius = getRadius(magnitude);
+        var color = getColor(magnitude);
+        var latlng = L.latLng([feature.geometry.coordinates[1], feature.geometry.coordinates[0]]) //
+        return L.circleMarker(latlng, circleStyle(color,radius));
+      } //*********************
+    }).addTo(myMap);
+
+    
+
+    
+
+    
+    // Map object, centered with default layers
+    
+
+    L.control.layers(baseMaps, overlayMaps, {
+      collapsed: false
+    }).addTo(myMap);
+
+    // Add legend here********************
+  }
+
+
+  // Perform a GET request to the query URL
+
+    // Once we get a response, send the data.features object to the createFeatures function.
+    console.log(data.features[0].geometry.coordinates[1])
+    createFeatures(data.features); //***********************
+}); 
+
+var myMap = L.map("map", {
+  center: [
+    37.09, -95.71
+  ],
+  zoom: 3,
+  // layers: [topo, earthquakes]
+});
+
+// Topographical map layer
+var topo = L.tileLayer('https://{s}.tile.opentopomap.org/{z}/{x}/{y}.png', {
   attribution: 'Map data: &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, <a href="http://viewfinderpanoramas.org">SRTM</a> | Map style: &copy; <a href="https://opentopomap.org">OpenTopoMap</a> (<a href="https://creativecommons.org/licenses/by-sa/3.0/">CC-BY-SA</a>)'
 })
+topo.addTo(myMap)
 
-let baseMaps = {
-  "Streets": streets,
-  "Topography": topo
+// Earthquake variable
+var earthquakes = L.layerGroup();
+earthquakes.addTo(myMap)
+
+// Street map layer
+var street = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+  attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+})
+
+// Base layer that holds both
+var baseMaps = {
+  "Topographic Map": topo,
+  "Street Map": street
 };
 
-// map object
-let map = L.map('map', {
-  center: [0, 0], // lat,lon,
-  zoom: 3
-});
-
-L.control.layers(baseMaps).addTo(map);
-
-function styleInfo(feature) {
-  return {
-    opacity: 1,
-    fillOpacity: 1,
-    fillColor: "#ffae42",
-    color: "#000000",
-    radius: getRadius(),
-    stroke: true,
-    weight: 0.5
-  };
-}
-function getRadius(magnitude){
-  if (magnitude === 0) {
-    return 1;
-  }
-  return magnitude * 4
-}
-
-// perform Get request to the query URL
-d3.json(queryUrl).then(function (data) {
-  L.geoJson(data. {
-    pointToLayer: function(feature, latlng) {
-      console.log(data);
-      return L.circleMarker(latlng);
-    },
-    // We set the style for each circleMarker using our styleInfo function.
-    style: styleInfo
-  }).addTo(map);
-});
-
-// add tile layer
-L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-  attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-}).addTo(map);
+var overlayMaps = {
+  "Earthquakes":earthquakes
+};
